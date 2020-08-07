589e8ae73db71d697c2c343ac2eb0d91b1e50609
bozhang213817 <bozhang213817@sohu-inc.com>
2020-08-07 12:09:12 +0800

    updated

--- 工具类脚本/操作redis.py
+++ 工具类脚本/操作redis.py
@@ -2,20 +2,17 @@
 # __Author__='Zhangbo'
 # __Time__='2018/12/6 15:57'
 
+
 import redis
-from prettyprinter import cpprint
 
-r = redis.Redis(host='mb.y.redis.sohucs.com', port=22939, password="b87418ff6e92b4db923e71b3eeaf9d7c", db=14)
-# r.hset("18611134450_headers", 'token',"eyJleHAiOjE1NTIyOTMyMzQwMzUsImlhdCI6MTU0NDUxNzIzNDAzNSwicHAiOiI4NzAxODkyMjU5NjkzODEzNzZAc29odS5jb20iLCJ0ayI6IjNrMnZPaFlkWERmMHRpR1NoWG12QWVnZmlsa2RSTHFIIiwidiI6MH0.oYhWKos-z77tMPDGpZXJcdIYYHK5uu697aCrvL-q2Mo")
-# r.hset("18611134450_headers", 'gid',"x011050101010dc565515ec36000080b5f6ec03f780f")
-# headers = {}
-# headers['token'] = r.hget("18611134450_headers", 'token').decode()
-# headers['gid'] = r.hget("18611134450_headers", 'gid').decode()
-#
-# cpprint(headers)
+current_date = "2020-08-06"
 
+r = redis.Redis(host='mb.y.redis.sohucs.com', port=22939, password="b87418ff6e92b4db923e71b3eeaf9d7c", db=4)
 
-r.set("test", "test")
-value_test = r.get("test").decode()
-print(value_test)
+# 写数据
+r.hset("{}".format(current_date), "android_dev", "{}".format("100%"))
+# 读数据
+# r.hget("{}".format(y), "a_created_inday").decode()
 
+# r.set("test", "test")
+# r.get("test").decode()
