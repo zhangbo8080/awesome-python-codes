8fb4fb000e94f18c9f025c17146a050c86c2bca2
bozhang213817 <bozhang213817@sohu-inc.com>
2020-08-07 15:33:19 +0800

    优化方法格式

--- 工具类脚本/操作redis.py
+++ 工具类脚本/操作redis.py
@@ -2,17 +2,22 @@
 # __Author__='Zhangbo'
 # __Time__='2018/12/6 15:57'
 
-
 import redis
-
-current_date = "2020-08-06"
-
-r = redis.Redis(host='mb.y.redis.sohucs.com', port=22939, password="b87418ff6e92b4db923e71b3eeaf9d7c", db=4)
+from prettyprinter import cpprint
 
 # 写数据
-r.hset("{}".format(current_date), "android_dev", "{}".format("100%"))
-# 读数据
-# r.hget("{}".format(y), "a_created_inday").decode()
+def set_redis(db, key, hash_key, hash_value):
+    r = redis.Redis(host='mb.y.redis.sohucs.com', port=22939, password="b87418ff6e92b4db923e71b3eeaf9d7c", db=db)
+    r.hset(key, hash_key, hash_value)
 
-# r.set("test", "test")
-# r.get("test").decode()
+# 读数据
+def get_redis(db, key):
+    r = redis.Redis(host='mb.y.redis.sohucs.com', port=22939, password="b87418ff6e92b4db923e71b3eeaf9d7c", db=db)
+    result = r.hgetall(key)
+    cpprint(result)
+
+if '__main__' == __name__:
+
+    db=4 #db=4是日报数据，db=7是需要变更
+    key="2020-08-07"
+    get_redis(db, key)
